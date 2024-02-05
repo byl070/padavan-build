45f5566b55772cffe586beff9bbde8b257b00c04
byl070 <61816285+byl070@users.noreply.github.com>
2024-02-05 12:37:01 +0800

    更新 wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -8,7 +8,7 @@ start_wg() {
 	presharedkey="$(nvram get wireguard_presharedkey)"
 	peerip="$(nvram get wireguard_peerip)"
 	logger -t "WIREGUARD" "正在启动wireguard"
-	ifconfig wg0 down
+	ip link set dev wg0 down
 	ip link del dev wg0
 	ip link add dev wg0 type wireguard
 	ip link set dev wg0 mtu 1420
@@ -18,12 +18,12 @@ start_wg() {
 	echo "$presharedkey" > /tmp/presharedkey
 	wg set wg0 peer $peerkey preshared-key /tmp/presharedkey persistent-keepalive 25 allowed-ips 0.0.0.0/0 endpoint $peerip
 	iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
-	ifconfig wg0 up
+	ip link set dev wg0 up
 }
 
 
 stop_wg() {
-	ifconfig wg0 down
+	ip link set dev wg0 down
 	ip link del dev wg0
 	logger -t "WIREGUARD" "正在关闭wireguard"
 	}
