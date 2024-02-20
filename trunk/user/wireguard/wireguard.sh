f7dd70172b0a9e7707ff90d40dcf321489b83a17
byl070 <61816285+byl070@users.noreply.github.com>
2024-02-20 16:22:08 +0800

    更新 wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -9,7 +9,8 @@ start_wg() {
 	peerip="$(nvram get wireguard_peerip)"
 	routeip="$(nvram get wireguard_routeip)"
 	if [ -z $localip ] || [ -z $privatekey ] || [ -z $peerkey ]; then
-		logger -t "WIREGUARD" "Config Error" && exit 0
+		logger -t "WIREGUARD" "Config Error"
+		echo "Wireguard Config Error" && exit 0
 	fi
 	ip link add dev wg0 type wireguard
 	ip link set dev wg0 mtu 1420
