a47c01932ed9c7e9c9ef5e24869d89913f48f975
byl070 <61816285+byl070@users.noreply.github.com>
2024-02-21 12:35:31 +0800

    更新 wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -8,18 +8,18 @@ start_wg() {
 	presharedkey="$(nvram get wireguard_prekey)"
 	peerip="$(nvram get wireguard_peerip)"
 	routeip="$(nvram get wireguard_routeip)"
-	[ -z $localip ] && err="LocalIP is empty"
-	[ -z $privatekey ] && err="Local PrivateKey is empty"
-	[ -z $peerkey ] && err="Peer PublicKey is empty"
+	[ ! $localip ] && err="LocalIP is empty"
+	[ ! $privatekey ] && [ ! $err ] && err="Local PrivateKey is empty"
+	[ ! $peerkey ] && [ ! $err ] && err="Peer PublicKey is empty"
 	[ "$err" ] && logger -t "WIREGUARD" "Start Error,$err" && exit "Start Error,$err"
 	ip link set dev wg0 down 2>/dev/null
 	ip link del dev wg0 2>/dev/null
 	ip link add dev wg0 type wireguard
 	ip link set dev wg0 mtu 1420
 	ip addr add $localip dev wg0
-	echo "$privatekey" > /tmp/privatekey && wg set wg0 private-key /tmp/privatekey
-	[ "$listenport" ] && wg set wg0 listen-port $listenport
-	[ "$presharedkey" ] && echo "$presharedkey" > /tmp/presharedkey && wg set wg0 peer $peerkey preshared-key /tmp/presharedkey
+	echo $privatekey > /tmp/privatekey && wg set wg0 private-key /tmp/privatekey
+	[ $listenport ] && wg set wg0 listen-port $listenport
+	[ $presharedkey ] && echo $presharedkey > /tmp/presharedkey && wg set wg0 peer $peerkey preshared-key /tmp/presharedkey
 	wg set wg0 peer $peerkey persistent-keepalive 30 allowed-ips 0.0.0.0/0 endpoint $peerip
 	ip link set dev wg0 up && logger -t "WIREGUARD" "Wireguard is Start"
 	iptables -N wireguard 2>/dev/null
