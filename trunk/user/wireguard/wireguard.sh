3a7af46694967259de62a0a0b034ff966fbfdaa5
byl070 <61816285+byl070@users.noreply.github.com>
2024-02-21 11:09:27 +0800

    更新 wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -33,7 +33,7 @@ start_wg() {
 
 stop_wg() {
 	if ip link set dev wg0 down 2>/dev/null && ip link del dev wg0; then
-	logger -t "WIREGUARD" "Wireguard is Stop"
+	 logger -t "WIREGUARD" "Wireguard is Stop"
 	 iptables -D INPUT -i wg0 -j wireguard 2>/dev/null
 	 iptables -D FORWARD -i wg0 -j wireguard 2>/dev/null
 	 iptables -F wireguard 2>/dev/null
