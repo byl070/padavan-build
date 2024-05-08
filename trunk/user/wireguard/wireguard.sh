6b600c4b31f00fd21532ba3f30ad21b5c8b96985
byl070 <61816285+byl070@users.noreply.github.com>
2024-05-08 19:00:03 +0800

    更新 wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -5,9 +5,9 @@ if [ ! -d $dir ]; then mkdir $dir; fi
 if [ ! -f $dir/wireguard.sh ]; then
 	echo "not found $dir/wireguard.sh"
 	logger -t "WireGuard" "not found $dir/wireguard.sh"
-else
-   chmod +x $dir/wireguard.sh
-   $dir/wireguard.sh $1
+	else
+chmod +x $dir/wireguard.sh
+$dir/wireguard.sh $1
 fi
 
 
